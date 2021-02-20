66bacc916bb37be51c71706f9ee51f1d6961f716
omnires <rivive@wp.pl>
2021-02-20 19:37:07 +0100

    initial commit 2

---
+++ src/main/java/com/checking/gitter/GitterApplication.java
@@ -0,0 +1,13 @@
+package com.checking.gitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitterApplication.class, args);
+	}
+
+}
